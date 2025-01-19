47e00ba7e5c8c7148989e37bbe41495f3a1a04c6
hz <beijingren@vip.qq.com>
2025-01-19 13:09:35 +0800

    调整编译配置

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,9 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven {
+            url = uri("https://repository.zmkn.com/repository/maven-public/")
+        }
         maven { url = uri("https://jitpack.io") }
         maven { url = uri("https://oss.sonatype.org/content/repositories/snapshots") }
         google()
