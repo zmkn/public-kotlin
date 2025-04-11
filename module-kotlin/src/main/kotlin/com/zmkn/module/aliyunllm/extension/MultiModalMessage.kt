88085ae5777a4f48f46b5dcdd7055582f1e1cbac
hz <beijingren@vip.qq.com>
2025-04-11 12:05:19 +0800

    调整 AliyunLlm 模块逻辑

--- module-kotlin/src/main/kotlin/com/zmkn/module/aliyunllm/extension/MultiModalMessage.kt
+++ module-kotlin/src/main/kotlin/com/zmkn/module/aliyunllm/extension/MultiModalMessage.kt
@@ -1,6 +1,7 @@
 package com.zmkn.module.aliyunllm.extension
 
 import com.alibaba.dashscope.common.MultiModalMessage
+import com.alibaba.dashscope.utils.JsonUtils
 import com.zmkn.module.aliyunllm.enumeration.MessageRole
 import com.zmkn.module.aliyunllm.model.MultiModalResponseMessage
 
@@ -9,7 +10,7 @@ fun MultiModalMessage.toMultiModalResponseMessageOutputChoiceMessage(): MultiMod
         role = MessageRole.fromValue(role),
         content = content.mapNotNull { map ->
             map.mapValues { (_, value) ->
-                value.toString()
+                JsonUtils.toJson(value)
             }
         },
         reasoningContent = reasoningContent,
