073b7539c2cd23c9b3d8dc94ba38df2afb81531b
hz <beijingren@vip.qq.com>
2025-04-14 13:09:45 +0800

    调整 AliyunLlm 模块逻辑

--- module-kotlin/src/main/kotlin/com/zmkn/module/aliyunllm/extension/ToolCallFunction.kt
+++ module-kotlin/src/main/kotlin/com/zmkn/module/aliyunllm/extension/ToolCallFunction.kt
@@ -6,5 +6,5 @@ import com.zmkn.module.aliyunllm.model.ResponseMessage
 fun ToolCallFunction.toResponseMessageOutputChoiceMessageToolCallFunction(): ResponseMessage.Output.Choice.Message.ToolCall.Function = ResponseMessage.Output.Choice.Message.ToolCall.Function(
     name = function.name,
     arguments = function.arguments,
-    output = function.output,
+    output = function.output ?: "",
 )
