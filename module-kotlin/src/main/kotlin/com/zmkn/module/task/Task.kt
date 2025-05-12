bd59b1f2ebf83669959f461966637076009713f6
hz <beijingren@vip.qq.com>
2025-05-12 10:12:27 +0800

    调整 Task 模块逻辑

---
+++ module-kotlin/src/main/kotlin/com/zmkn/module/task/Task.kt
@@ -0,0 +1,162 @@
+package com.zmkn.module.task
+
+import com.zmkn.module.task.enumeration.TaskStatus
+import com.zmkn.util.RandomUtils
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+
+data class Task(
+    val id: String,
+    val job: Job,
+    val name: String,
+    val status: TaskStatus,
+    val desc: String? = null,
+    val result: Any? = null,
+) {
+    fun success() = success(id)
+
+    fun cancel() = cancel(id)
+
+    fun remove() = remove(id)
+
+    companion object {
+        private val _jobs: MutableMap<String, Task> = mutableMapOf()
+        private val _supervisorScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+
+        private fun generateTaskId(): String {
+            return "task-${RandomUtils.generateRandomNumber(12)}"
+        }
+
+        val jobs: Map<String, Task> = _jobs
+
+        fun getTask(id: String): Task? {
+            return _jobs[id]
+        }
+
+        fun create(
+            name: String,
+            desc: String? = null,
+            delayMillis: Long? = null,
+            block: suspend (task: Task) -> Any?
+        ): String {
+            val id = generateTaskId()
+            return _supervisorScope.launch {
+                try {
+                    val task = getTask(id)!!
+                    delayMillis?.let {
+                        delay(it)
+                    }
+                    val result = block(task)
+                    _jobs[id]?.also {
+                        _jobs[id] = it.copy(
+                            status = TaskStatus.SUCCEEDED,
+                            result = result,
+                        )
+                    }
+                } catch (e: Exception) {
+                    _jobs[id]?.also {
+                        if (!it.job.isCancelled) {
+                            _jobs[id] = it.copy(
+                                status = TaskStatus.FAILED,
+                                result = e,
+                            )
+                        }
+                    }
+                }
+            }.let {
+                _jobs[id] = Task(
+                    id = id,
+                    job = it,
+                    name = name,
+                    status = TaskStatus.NORMAL,
+                    desc = desc,
+                    result = null,
+                )
+                id
+            }
+        }
+
+        fun createInterval(
+            name: String,
+            desc: String? = null,
+            period: Long,
+            immediate: Boolean = true,
+            block: suspend (task: Task) -> Any?
+        ): String {
+            val id = generateTaskId()
+            return _supervisorScope.launch {
+                try {
+                    val task = getTask(id)!!
+                    if (immediate) {
+                        block(task)
+                    }
+                    while (isActive) {
+                        delay(period)
+                        block(task)
+                    }
+                } catch (e: Exception) {
+                    _jobs[id]?.also {
+                        if (!it.job.isCancelled) {
+                            _jobs[id] = it.copy(
+                                status = TaskStatus.FAILED,
+                                result = e,
+                            )
+                        }
+                    }
+                }
+            }.let {
+                _jobs[id] = Task(
+                    id = id,
+                    job = it,
+                    name = name,
+                    status = TaskStatus.NORMAL,
+                    desc = desc,
+                    result = null,
+                )
+                id
+            }
+        }
+
+        fun success(id: String) {
+            _jobs[id]?.also {
+                it.job.cancel()
+                _jobs[id] = it.copy(
+                    status = TaskStatus.SUCCEEDED
+                )
+            }
+        }
+
+        fun cancel(id: String) {
+            _jobs[id]?.also {
+                it.job.cancel()
+                _jobs[id] = it.copy(
+                    status = TaskStatus.CANCELED
+                )
+            }
+        }
+
+        fun remove(id: String) {
+            cancel(id)
+            _jobs.remove(id)
+        }
+
+        fun cancelAll() {
+            _jobs.forEach { id, task ->
+                task.job.cancel()
+                _jobs[id] = task.copy(
+                    status = TaskStatus.CANCELED
+                )
+            }
+        }
+
+        fun removeAll() {
+            cancelAll()
+            _jobs.clear()
+        }
+    }
+}
--- module-kotlin/src/main/kotlin/com/zmkn/module/task/util/TaskUtils.kt
+++ module-kotlin/src/main/kotlin/com/zmkn/module/task/util/TaskUtils.kt
@@ -1,140 +1,45 @@
 package com.zmkn.module.task.util
 
-import com.zmkn.module.task.enumeration.TaskStatus
-import com.zmkn.module.task.model.Task
-import com.zmkn.util.RandomUtils
-import kotlinx.coroutines.*
+import com.zmkn.module.task.Task
 
 object TaskUtils {
-    private val _jobs: MutableMap<String, Task> = mutableMapOf()
-    private val _supervisorScope = CoroutineScope(SupervisorJob() + Dispatchers.IO)
+    val jobs = Task.jobs
 
-    val jobs: Map<String, Task> = _jobs
-
-    private fun generateTaskId(): String {
-        return "task-${RandomUtils.generateRandomNumber(12)}"
-    }
-
-    fun getJob(id: String): Job? {
-        return _jobs[id]?.job
-    }
+    fun getTask(id: String) = Task.getTask(id)
 
     fun create(
         name: String,
         desc: String? = null,
         delayMillis: Long? = null,
-        block: suspend (coroutineScope: CoroutineScope) -> Any?
-    ): String {
-        val id = generateTaskId()
-        return _supervisorScope.launch {
-            try {
-                delayMillis?.let {
-                    delay(it)
-                }
-                val result = block(this)
-                _jobs[id]?.also {
-                    _jobs[id] = it.copy(
-                        status = TaskStatus.SUCCEEDED,
-                        result = result,
-                    )
-                }
-            } catch (e: Exception) {
-                _jobs[id]?.also {
-                    if (!it.job.isCancelled) {
-                        _jobs[id] = it.copy(
-                            status = TaskStatus.FAILED,
-                            result = e,
-                        )
-                    }
-                }
-            }
-        }.let {
-            _jobs[id] = Task(
-                id = id,
-                job = it,
-                name = name,
-                status = TaskStatus.NORMAL,
-                desc = desc,
-                result = null,
-            )
-            id
-        }
-    }
+        block: suspend (task: Task) -> Any?
+    ) = Task.create(
+        name = name,
+        desc = desc,
+        delayMillis = delayMillis,
+        block = block,
+    )
 
     fun createInterval(
         name: String,
         desc: String? = null,
         period: Long,
         immediate: Boolean = true,
-        block: suspend (coroutineScope: CoroutineScope) -> Any?
-    ): String {
-        val id = generateTaskId()
-        return _supervisorScope.launch {
-            try {
-                if (immediate) {
-                    block(this)
-                }
-                while (isActive) {
-                    delay(period)
-                    block(this)
-                }
-            } catch (e: Exception) {
-                _jobs[id]?.also {
-                    if (!it.job.isCancelled) {
-                        _jobs[id] = it.copy(
-                            status = TaskStatus.FAILED,
-                            result = e,
-                        )
-                    }
-                }
-            }
-        }.let {
-            _jobs[id] = Task(
-                id = id,
-                job = it,
-                name = name,
-                status = TaskStatus.NORMAL,
-                desc = desc,
-                result = null,
-            )
-            id
-        }
-    }
+        block: suspend (task: Task) -> Any?
+    ) = Task.createInterval(
+        name = name,
+        desc = desc,
+        period = period,
+        immediate = immediate,
+        block = block,
+    )
 
-    fun success(id: String) {
-        _jobs[id]?.also {
-            it.job.cancel()
-            _jobs[id] = it.copy(
-                status = TaskStatus.SUCCEEDED
-            )
-        }
-    }
+    fun success(id: String) = Task.success(id)
 
-    fun cancel(id: String) {
-        _jobs[id]?.also {
-            it.job.cancel()
-            _jobs[id] = it.copy(
-                status = TaskStatus.CANCELED
-            )
-        }
-    }
+    fun cancel(id: String) = Task.cancel(id)
 
-    fun cancelAll() {
-        _jobs.forEach { id, task ->
-            task.job.cancel()
-            _jobs[id] = task.copy(
-                status = TaskStatus.CANCELED
-            )
-        }
-    }
+    fun remove(id: String) = Task.remove(id)
 
-    fun remove(id: String) {
-        cancel(id)
-        _jobs.remove(id)
-    }
+    fun cancelAll() = Task.cancelAll()
 
-    fun removeAll() {
-        cancelAll()
-        _jobs.clear()
-    }
+    fun removeAll() = Task.removeAll()
 }
