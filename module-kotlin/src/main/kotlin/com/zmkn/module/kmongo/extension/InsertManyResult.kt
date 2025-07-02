d20e2cee4408674a51570db97347792044bc8cf1
hz <beijingren@vip.qq.com>
2025-07-02 16:08:32 +0800

    调整 KMongo 模块逻辑

--- module-kotlin/src/main/kotlin/com/zmkn/module/kmongo/extension/InsertManyResult.kt
+++ module-kotlin/src/main/kotlin/com/zmkn/module/kmongo/extension/InsertManyResult.kt
@@ -2,4 +2,4 @@ package com.zmkn.module.kmongo.extension
 
 import com.mongodb.client.result.InsertManyResult
 
-fun InsertManyResult.isSuccessful(): Boolean = wasAcknowledged() && this.insertedIds != null
+fun InsertManyResult.isSuccessful(): Boolean = wasAcknowledged() && insertedIds.isNotEmpty()
